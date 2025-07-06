95f741ba8f8d5699daaad3d2cab784465e8ba96c
Suana Mešić <suana.mesic@edu.fit.ba>
2025-07-06 15:11:06 +0200

    Postavka

---
+++ 2025-06-20/[20.06.2025] Postavka.sql
@@ -0,0 +1,17 @@
+--Ispit 20.06.2025. Grupa B :: Postavka
+--Napomena: Postavka nije potpuna (sadrži samo neke od zadataka)
+
+--Baza: AdventureWorks2017
+--a) (5 bodova) Prikazati sve narudžbe iz 2011. godine koje sadrže tačno jedan proizvod. Zaglavlje (kolone): ID narudžbe
+--b) (7 bodova) Prikazati ukupni prihod (količina * cijena) i broj narudžbi po kupcu i godini kupovine. Zaglavlje: Kupac, Godina kupovine, Prihod, Broj narudžbi
+--c) (8 bodova) Prikazati sve proizvode koji nisu nikada naručeni, imaju cijenu veću od 100 i nalaze se u skladištu u količini većoj od 800 komada. Rezultate sortirati po cijeni u opadajućem redoslijedu- Zaglavlje: Naziv proizvoda, Cijena, Količina
+
+
+--Baza: Northwind
+--a) (4 boda) Prikazati državu iz koje su narudbe isporučene najbrže (najmanje prosječno vrijeme isporuke) Zaglavlje: Država, Prosječan broj dana.
+--b) (7 bodova) Prikazati kupce čije su sve narudžbe isporučene u roku kraćem od 5 dana. Zaglavlje: Naziv kompanije kupca
+--c) (7 bodova) Prikazati kupce koji su naručili samo jednom, i to proizvode kojih ima na stanju u količini manjoj od 20. Zaglavlje: Naziv kompanije kupca.
+
+--Baza: pubs
+--a) (10 bodova) Prikazati naslove koji se nisu prodali nijednom u trgovinama koje su prodale više od 5 različitih naslova. Zaglavlje: Naslov knjige
+--b) (10 bodova) Prikazati naslove koji su se prodali isključivo u onim godinama u kojima ukupna prodaja svih naslova nije prelazila 80 primjeraka. Zaglavlje: title(naslov knjige)
